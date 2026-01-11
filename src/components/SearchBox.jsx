916f9023e95ba9a7321829b8ac9c84af6e398014
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-01-11 17:01:01 +0530

    add weather fetching functionality to SearchBox component

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -6,6 +6,26 @@ import '../Css/SearchBox.css'
 
 function SearchBox() {
   const [city, setcity] = useState("")
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather?"
+  const API_KEY = "fa4913e9435c45c042df3747cf825057";
+
+  let getWeatherInfo= async()=>{
+   let response = await fetch (`${API_URL}q=${city}&appid=${API_KEY}&units=metric`);
+    let data = await response.json();
+    console.log(data)
+    let result = {
+      temp: data.main.temp,
+      tempMin: data.main.temp_min,
+      tempMax: data.main.temp_max,
+      humidity: data.main.humidity,
+      pressure: data.main.pressure,
+      weatherType: data.weather[0].main,
+      cityName: data.name,
+      windSpeed: data.wind.speed,
+      country: data.sys.country,
+      sunsetTime: data.sys.sunset,
+    };
+  }
 
   let handleChange=(event)=>{
     setcity(event.target.value);
@@ -14,6 +34,7 @@ function SearchBox() {
     event.preventDefault();
     console.log("Form Submitted -> ", city);
     setcity("");
+    getWeatherInfo();
   }
   return (
     <div className='SearchBox'>
