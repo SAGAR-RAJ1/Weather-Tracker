9f97f15583525969ccd3746143c8fc19e07cec3e
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-01-11 23:27:50 +0530

    fix: update API key handling in SearchBox and add environment variable for weather API

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,10 +4,11 @@ import Button from '@mui/material/Button'
 import '../Css/SearchBox.css'
 
 
+
 function SearchBox({UpdateWeather}) {
   const [city, setcity] = useState("")
   const API_URL = "https://api.openweathermap.org/data/2.5/weather?"
-  const API_KEY = "fa4913e9435c45c042df3747cf825057";
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
   let getWeatherInfo= async()=>{
    let response = await fetch (`${API_URL}q=${city}&appid=${API_KEY}&units=metric`);
